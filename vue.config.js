fd4d2033ff3123a550e8901bcada081d74e29627
Dobrin Dimchev <dobrin.yonkov1@gmail.com>
2021-04-03 22:53:15 +0300

    add stagin mode

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
-  publicPath: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'staging' 
   ? '/pagesandbox/'
   : '/',
 }
